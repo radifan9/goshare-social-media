c75115ea9ee20bae60d3708215b138e982303a87
radifan9 <radif.aghnadiin@gmail.com>
2025-11-08 16:21:17 +0700

    feat: add create user method

---
+++ internal/model/users.model.go
@@ -0,0 +1,9 @@
+package model
+
+type User struct {
+	ID        int64  `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Password  string `json:"-"`
+	CreatedAt string `json:"created_at"`
+}
--- internal/store/users.go
+++ internal/store/users.go
@@ -3,12 +3,31 @@ package store
 import (
 	"context"
 	"database/sql"
+	"social/internal/model"
 )
 
 type UsersStore struct {
 	db *sql.DB
 }
 
-func (s *UsersStore) Create(ctx context.Context) error {
+func (s *UsersStore) Create(ctx context.Context, user *model.User) error {
+	query := `
+		INSERT INTO users (username, password, email)
+		VALUES ($1, $2, $3) RETURNING id, created_at
+	`
+
+	if err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password,
+		user.Email,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	); err != nil {
+		return err
+	}
+
 	return nil
 }
