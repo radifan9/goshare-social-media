438be5129cf684b20a2d16da929580d695c232ff
radifan9 <radif.aghnadiin@gmail.com>
2025-11-08 18:55:32 +0700

    feat: add database abstraction

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"social/internal/db"
 	"social/internal/env"
 	"social/internal/store"
 )
@@ -10,9 +11,27 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/goshare?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+	log.Println("database connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
---
+++ internal/db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+// Here we are not passing the config as a struct
+// Because internal packages should not know about the external world
+func New(addr string, maxOpenConns, maxIdleConns int, maxIdleTime string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+	duration, err := time.ParseDuration(maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+
+	// Cancel if it takes more than 5 sec to connect
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
