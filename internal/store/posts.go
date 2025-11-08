6c6a8a0072604752ec854253df14a6a412bb302e
radifan9 <radif.aghnadiin@gmail.com>
2025-11-08 16:21:03 +0700

    feat: add create post method

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,12 +3,35 @@ package store
 import (
 	"context"
 	"database/sql"
+	"social/internal/model"
+
+	"github.com/lib/pq"
 )
 
-type PostsStore struct {
+type PostStore struct {
 	db *sql.DB
 }
 
-func (s *PostsStore) Create(ctx context.Context) error {
+func (s *PostStore) Create(ctx context.Context, post *model.Post) error {
+	query := `
+		INSERT INTO posts (content, title, user_id, tags)
+		VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
+		`
+
+	if err := s.db.QueryRowContext(
+		ctx,
+		query,
+		post.Content,
+		post.Title,
+		post.UserID,
+		pq.Array(post.Tags),
+	).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	); err != nil {
+		return err
+	}
+
 	return nil
 }
--- internal/store/storage.go
+++ internal/store/storage.go
@@ -3,21 +3,22 @@ package store
 import (
 	"context"
 	"database/sql"
+	"social/internal/model"
 )
 
 type Storage struct {
 	Posts interface {
-		Create(context.Context) error
+		Create(context.Context, *model.Post) error
 	}
 
 	Users interface {
-		Create(context.Context) error
+		Create(context.Context, *model.User) error
 	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts: &PostsStore{db},
+		Posts: &PostStore{db},
 		Users: &UsersStore{db},
 	}
 }
