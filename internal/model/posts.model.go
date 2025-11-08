d0bfaf320b86e85086bff10e21fdd4b9eb0b64cd
radifan9 <radif.aghnadiin@gmail.com>
2025-11-08 16:28:17 +0700

    feat: change data type of timestamp

--- internal/model/posts.model.go
+++ internal/model/posts.model.go
@@ -1,11 +1,13 @@
 package model
 
+import "time"
+
 type Post struct {
-	ID        int64    `json:"id"`
-	UserID    int64    `json:"user_id"`
-	Tags      []string `json:"tags"`
-	Title     string   `json:"title"`
-	Content   string   `json:"content"`
-	CreatedAt string   `json:"created_at"` // could be time.Time
-	UpdatedAt string   `json:"updated_at"`
+	ID        int64     `json:"id"`
+	UserID    int64     `json:"user_id"`
+	Tags      []string  `json:"tags"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
--- internal/model/users.model.go
+++ internal/model/users.model.go
@@ -1,9 +1,11 @@
 package model
 
+import "time"
+
 type User struct {
-	ID        int64  `json:"id"`
-	Username  string `json:"username"`
-	Email     string `json:"email"`
-	Password  string `json:"-"`
-	CreatedAt string `json:"created_at"`
+	ID        int64     `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
 }
